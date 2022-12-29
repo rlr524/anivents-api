52b8c8198778067839811073746e8ee20c5fc8ba
Rob Ranf <rob@emiyaconsulting.com>
2022-12-28 18:57:18 -0800

    activity domain

---
+++ Domain/Activity.cs
@@ -0,0 +1,16 @@
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZIP { get; set; }
+        public string Venue { get; set; }
+    }
+}
