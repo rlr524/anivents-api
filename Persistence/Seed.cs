2157ab03b44544c7a3141986e8bec4672d5f4d86
Rob Ranf <rob@emiyaconsulting.com>
2023-01-04 14:49:19 -0800

    activities controller

---
+++ Persistence/Seed.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Activities.Any()) return;
+
+            var activities = new List<Activity>
+            {
+                new Activity
+                {
+                    Title = "Past Activity 1",
+                    Date = DateTime.UtcNow.AddMonths(-2),
+                    Description = "Activity 2 months ago",
+                    Category = "drinks",
+                    Street = "22 Mayfair",
+                    City = "London",
+                    State = "",
+                    ZIP = "",
+                    Venue = "Pub",
+                },
+                new Activity
+                {
+                    Title = "Past Activity 2",
+                    Date = DateTime.UtcNow.AddMonths(-1),
+                    Description = "Activity 1 month ago",
+                    Category = "culture",
+                    Street = "",
+                    City = "Paris",
+                    State = "",
+                    ZIP = "",
+                    Venue = "Louvre",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 1",
+                    Date = DateTime.UtcNow.AddMonths(1),
+                    Description = "Activity 1 month in future",
+                    Category = "culture",
+                    Street = "",
+                    City = "London",
+                    State = "",
+                    ZIP = "",
+                    Venue = "Natural History Museum",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 2",
+                    Date = DateTime.UtcNow.AddMonths(2),
+                    Description = "Activity 2 months in future",
+                    Category = "music",
+                    Street = "",
+                    City = "London",
+                    State = "",
+                    ZIP = "",
+                    Venue = "O2 Arena",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 3",
+                    Date = DateTime.UtcNow.AddMonths(3),
+                    Description = "Activity 3 months in future",
+                    Category = "drinks",
+                    Street = "",
+                    City = "London",
+                    State = "",
+                    ZIP = "",
+                    Venue = "Another pub",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 4",
+                    Date = DateTime.UtcNow.AddMonths(4),
+                    Description = "Activity 4 months in future",
+                    Category = "drinks",
+                    Street = "",
+                    City = "London",
+                    State = "",
+                    ZIP = "",
+                    Venue = "Yet another pub",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 5",
+                    Date = DateTime.UtcNow.AddMonths(5),
+                    Description = "Activity 5 months in future",
+                    Category = "drinks",
+                    Street = "",
+                    City = "London",
+                    State = "",
+                    ZIP = "",
+                    Venue = "Just another pub",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 6",
+                    Date = DateTime.UtcNow.AddMonths(6),
+                    Description = "Activity 6 months in future",
+                    Category = "music",
+                    Street = "",
+                    City = "London",
+                    State = "",
+                    ZIP = "",
+                    Venue = "Roundhouse Camden",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 7",
+                    Date = DateTime.UtcNow.AddMonths(7),
+                    Description = "Activity 2 months ago",
+                    Category = "travel",
+                    Street = "",
+                    City = "London",
+                    State = "",
+                    ZIP = "",
+                    Venue = "Somewhere on the Thames",
+                },
+                new Activity
+                {
+                    Title = "Future Activity 8",
+                    Date = DateTime.UtcNow.AddMonths(8),
+                    Description = "Activity 8 months in future",
+                    Category = "film",
+                    Street = "",
+                    City = "London",
+                    State = "",
+                    ZIP = "",
+                    Venue = "Cinema",
+                }
+            };
+
+            await context.Activities.AddRangeAsync(activities); // Saves the changes into memory
+            await context.SaveChangesAsync(); // Commits the changes to the db
+        }
+    }
+}
